a78cd9fc09ccb5d1333fa0264fbc545359131151
ehiggi <ehiggins@blackhillsip.com>
2024-11-25 12:59:48 -0600

    Attempt to pass ssl_cert_reqs through to rq_dashboard

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import os
 from flask import Flask, redirect
-import redis
 import rq_dashboard
 from rq_dashboard.cli import add_basic_auth
+
+
+def setup_rq_connection(current_app):
+    # we need to do It here instead of cli, since It may be embeded
+    rq_dashboard.web.upgrade_config(current_app)
+    # Getting Redis connection parameters for RQ
+    redis_url = current_app.config.get("RQ_DASHBOARD_REDIS_URL")
+    if isinstance(redis_url, rq_dashboard.web.string_types):
+        current_app.config["RQ_DASHBOARD_REDIS_URL"] = (redis_url,)
+        _, current_app.redis_conn = rq_dashboard.web.from_url(
+            (redis_url,)[0], client_options={"ssl_cert_reqs": None}
+        )
+    elif isinstance(redis_url, (tuple, list)):
+        _, current_app.redis_conn = rq_dashboard.web.from_url(
+            redis_url[0], client_options={"ssl_cert_reqs": None}
+        )
+    else:
+        raise RuntimeError("No Redis configuration!")
+
 
 app = Flask(__name__)
 app.config.from_object(rq_dashboard.default_settings)
 app.config["RQ_DASHBOARD_REDIS_URL"] = os.environ.get("RQ_DASHBOARD_REDIS_URL")
 
-app.redis_conn = redis.StrictRedis.from_url(  # type: ignore
-    os.environ.get("RQ_DASHBOARD_REDIS_URL", ""), ssl_cert_reqs=None
-)
+setup_rq_connection(app)
 
 add_basic_auth(
     blueprint=rq_dashboard.blueprint,
